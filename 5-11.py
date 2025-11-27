80773adb7a03cbba668bde18e41e422bec0c5247
MahdyarTlb <talebimahdyar8@gmail.com>
2025-11-27 14:52:46 +0330

    end of 5

---
+++ 5-11.py
@@ -0,0 +1,32 @@
+## approach:
+# if  the exit stack is empty, multi pop the first stack
+# and push it into the exit stack
+# now pop can be like dequeue
+
+class Queue:
+    def __init__(self):
+        self.in_stack = []
+        self.out_stack = []
+
+    def add(self, x):
+        self.in_stack.append(x)
+
+    def remove(self):
+        if not self.out_stack:
+            while self.in_stack:
+                self.out_stack.append(self.in_stack.pop())
+        
+        return self.out_stack.pop()
+
+    def peek(self):
+        if not self.out_stack:
+            while self.in_stack:
+                self.out_stack.append(self.in_stack.pop())
+        return self.out_stack[-1] if self.out_stack else "خالیه!"
+
+    def is_empty(self):
+        return len(self.in_stack) == 0 and len(self.out_stack) == 0
+
+    def __str__(self):
+        temp = self.out_stack[::-1] + self.in_stack
+        return f"صف: {' ← '.join(map(str, temp)) if temp else 'خالی'} (اول ← آخر)"
---
+++ 5-12.py
@@ -0,0 +1,47 @@
+# approach:
+# we have a main queue and a temp one.
+# push approach is easy, enqueue to the queue
+# for pop, dequeue all of data in the main queue to temp queue except the last one,
+# then return the last one, (like stack(FILO))
+
+
+from collections import deque
+
+class StackWithTwoQueues:
+    def __init__(self):
+        self.main = deque()
+        self.temp = deque()
+
+    def push(self, x):
+        self.main.append(x)
+        print(f"اضافه شد: {x} → پشته: {list(self.main)}")
+
+    def pop(self):
+        if not self.main:
+            print("پشته خالیه !")
+            return None
+
+        while len(self.main) > 1:
+            self.temp.append(self.main.popleft())
+
+        top = self.main.popleft()
+        print(f"حذف شد: {top} (آخرین چیزی که اومده بود)")
+
+        while self.temp:
+            self.main.append(self.temp.popleft())
+
+        return top
+
+    def top(self):
+        if not self.main:
+            return None
+        return self.main[-1]
+
+    def is_empty(self):
+        return len(self.main) == 0
+
+    def size(self):
+        return len(self.main)
+
+    def __str__(self):
+        return f"پشته: {list(self.main)} (ته ← بالا)"
